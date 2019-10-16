1a3e8eca257e9e14001efc86d4f3c390a6f908d1
Char <cxjava@users.noreply.github.com>
2019-10-16 23:22:49 +0800

    Update upx.sh

--- upx.sh
+++ upx.sh
@@ -2,10 +2,5 @@
 
 set -euo pipefail
 
-case "$(uname -s)" in
-    "Darwin") brew install upx ;;
-    "Linux") curl -sL https://github.com/upx/upx/releases/download/v3.95/upx-3.95-amd64_linux.tar.xz | tar xv -C "$GOPATH/bin" --strip-components=1 upx-3.95-amd64_linux/upx ;;
-    *) echo "Sorry no support your OS yet" && exit 1 ;;
-esac
 
 find dist/ -type f -iname AriaNg-go | xargs -I{} -n1 -P 4 upx --best "{}"
