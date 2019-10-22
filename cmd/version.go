8ce572843de9b1751d792f51ed0673e4cd6376b2
Charles <cxjava@users.noreply.github.com>
2019-10-22 18:05:21 +0800

    update version [release]

--- cmd/version.go
+++ cmd/version.go
@@ -23,11 +23,11 @@ var versionCmd = &cobra.Command{
 	Long:    `Display the version.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Version: \t" + Version)
+		fmt.Println("Build Time: \t" + BuildDate)
+		fmt.Println("Go Version: \t" + BuiltWithGoVersion)
 		fmt.Println("Repo URL: \t" + RepoUrl)
 		fmt.Println("Commit Info: \t" + Commit)
-		fmt.Println("Build Time: \t" + BuildDate)
 		fmt.Println("Build By: \t" + BuiltBy)
-		fmt.Println("Go Version: \t" + BuiltWithGoVersion)
 	},
 }
 
